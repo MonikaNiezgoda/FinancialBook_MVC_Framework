41aad146c97549c18ff3da1215e35740dbdcdab4
MonikaNiezgoda <m.niezgoda.programista@gmail.com>
2022-12-03 14:39:57 +0100

    Add menu class

---
+++ App/Controllers/Menu.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+class Menu extends \Core\Controller
+{
+    public function mainAction()
+
+    {
+        View::renderTemplate('Menu/main.html');
+    }
+
+
+}
