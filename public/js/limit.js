4ebd4b3bd19013ef908370adbd854af76154e2e1
MonikaNiezgoda <m.niezgoda.programista@gmail.com>
2023-02-25 17:10:26 +0100

    Fix bugs in collapse

--- public/js/limit.js
+++ public/js/limit.js
@@ -1,6 +1,6 @@
 
 const getLimitCategory = (id) => {
-  return fetch(`https:///api/limit/${id}`)
+  return fetch(`https://monika-niezgoda.profesjonalnyprogramista.pl/api/limit/${id}`)
     .then(response => response.json())
     .then(data => {
       return data.category_limit;
@@ -11,7 +11,7 @@ const getLimitCategory = (id) => {
 };
 
 const getMonthExpenses = (id, date) => {
-  return fetch(`https:///date/${id}?date=${date}`)
+  return fetch(`https://monika-niezgoda.profesjonalnyprogramista.pl/date/${id}?date=${date}`)
     .then(response => response.json())
     .then(data => {
       return data;
@@ -24,14 +24,26 @@ const getMonthExpenses = (id, date) => {
 const countLimitLeft = (limit, data) => {
   const newExpense = $("#kwota").val();
   const myAlert = $("#myAlert");
-  const totalLeft = totalLeft = limit - newExpense;
-  
-  if (totalLeft > 0) {
-      myAlert.removeClass("alert-danger").addClass("alert-success");
-      myAlert.html(`<strong>Uwaga!</strong> <br/> Pozostało Ci jeszcze ${totalLeft} zł z limitu w wybranym miesiącu`);
-    } else if(totalLeft < 0) {
-      myAlert.removeClass("alert-success").addClass("alert-danger");
-      myAlert.html(`<strong>Uwaga!</strong> W wybranym miesiącu limit został już wyczerpany! </br> Przekroczyłeś limit o <strong>${Math.abs(totalLeft)} zł </strong> `);
+  let totalLeft = limit - newExpense - data;
+  console.log(limit);
+  console.log('test');
+    if(limit>0)
+    {
+      if (totalLeft > 0) {
+        myAlert.removeClass("alert-danger").addClass("alert-success");
+        myAlert.removeClass("alert-warning").addClass("alert-success");
+        myAlert.html(`<strong>Uwaga!</strong> <br/> Pozostało Ci jeszcze ${totalLeft} zł z limitu w wybranym miesiącu.`);
+      } else if(totalLeft < 0) {
+        myAlert.removeClass("alert-success").addClass("alert-danger");
+        myAlert.removeClass("alert-warning").addClass("alert-danger");
+        myAlert.html(`<strong>Uwaga!</strong> W wybranym miesiącu limit został już wyczerpany! </br> Przekroczyłeś limit o <strong>${Math.abs(totalLeft)} zł. </strong> `);
+      }
+    }
+    else
+    {
+      myAlert.removeClass("alert-danger").addClass("alert-warning");
+      //myAlert.removeClass("alert-success").addClass("alert-warning");
+      myAlert.html(`Na danej kategorii nie ma ustawionego limitu.`);
     }
   }
 
